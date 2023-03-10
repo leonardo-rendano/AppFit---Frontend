473446c5646aef84e78728b2c0b9edb582e0c602
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-03-10 13:47:45 -0300

    navigation menu component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
+import { NavigationMenu } from "../NavigationMenu"
+
 export const Header = () => {
   return (
-    <header className="w-5/6 m-auto border-b-2 h-20 flex items-center ">
+    <header className="w-5/6 m-auto border-b-2 h-20 flex items-center justify-between">
       <h1 className="text-3xl text-black font-semibold">
         App
         <span className="text-orange-300">
           Fit
         </span>
       </h1>
+
+      <NavigationMenu />
     </header>
   )
 }
---
+++ src/components/NavigationMenu/index.tsx
@@ -0,0 +1,12 @@
+export const NavigationMenu = () => {
+  return (
+    <nav className="w-1/3">
+      <ul className="flex items-center justify-between">
+        <li>Professores</li>
+        <li>Alunos</li>
+        <li>Séries</li>
+        <li>Exercícios</li>
+      </ul>
+    </nav>
+  )
+}
