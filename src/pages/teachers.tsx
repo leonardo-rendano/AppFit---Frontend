671ffc0d730f9593e2649f10cc29b491cb2d7397
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-03-10 17:46:43 -0300

    temporary form component created

--- src/pages/teachers.tsx
+++ src/pages/teachers.tsx
@@ -1,7 +1,86 @@
 export default function TeachersPage() {
   return (
     <div className="w-5/6 m-auto mt-20">
-      <h1>Professores</h1>
+      <div className="py-4">
+        <h1 className=" text-2xl">Professores</h1>
+      </div>
+
+      <div className=" mt-10 sm:mt-0 ">
+        <div className="md:grid md:grid-cols-2 md:gap-6">
+          <div className="mt-5 md:col-span-2 md:mt-0">
+            <form>
+              <div className="overflow-hidden shadow sm:rounded-md">
+                <div className="bg-white px-4 py-5 sm:p-6">
+                  <div className="grid grid-cols-6 gap-6">
+
+                    <div className="col-span-6 sm:col-span-3">
+                      <label htmlFor="nome" className="block text-sm font-medium leading-6 text-gray-900">
+                        Nome
+                      </label>
+                      <input
+                        type="text"
+                        name="nome"
+                        id="nome"
+                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+
+                    <div className="col-span-6 sm:col-span-3">
+                      <label htmlFor="registro" className="block text-sm font-medium leading-6 text-gray-900">
+                        Registro
+                      </label>
+                      <input
+                        type="text"
+                        name="registro"
+                        id="registro"
+                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+
+                    <div className="col-span-6 sm:col-span-3">
+                      <label htmlFor="cpf" className="block text-sm font-medium leading-6 text-gray-900">
+                        CPF
+                      </label>
+                      <input
+                        type="text"
+                        name="cpf"
+                        id="cpf"
+                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+
+                    <div className="col-span-6 sm:col-span-3">
+                      <label htmlFor="turno" className="block text-sm font-medium leading-6 text-gray-900">
+                        Turno
+                      </label>
+                      <select
+                        id="turno"
+                        name="turno"
+                        className="mt-2 block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      >
+                        <option>Manhã</option>
+                        <option>Tarde</option>
+                        <option>Noite</option>
+                      </select>
+                    </div>
+
+                  </div>
+
+                </div>
+                <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
