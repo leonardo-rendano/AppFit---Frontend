a4e91611857e7f8b6ebe2a432fa7dffa6b6511f4
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-03-13 13:27:57 -0300

    menuItems component refactor and students page finish

--- src/components/NavigationMenu/index.tsx
+++ src/components/NavigationMenu/index.tsx
@@ -1,29 +1,38 @@
 import Link from "next/link"
 
 export const NavigationMenu = () => {
+
+  const menuItems = [
+    {
+      href: '/teachers',
+      children: 'Professores'
+    },
+    {
+      href: '/students',
+      children: 'Alunos'
+    },
+    {
+      href: '/sets',
+      children: 'Séries'
+    },
+    {
+      href: '/exercises',
+      children: 'Exercícios'
+    }
+  ]
+
   return (
     <nav className="w-1/3">
       <ul className="flex items-center justify-between font-medium">
-        <li>
-          <Link href='/teachers'>
-            Professores
-          </Link>
-        </li>
-        <li>
-          <Link href='/students'>
-            Alunos
-          </Link>
-        </li>
-        <li>
-          <Link href='/sets'>
-            Séries
-          </Link>
-        </li>
-        <li>
-          <Link href='/exercises'>
-            Exercícios
-          </Link>
-        </li>
+        {menuItems.map(item => {
+          return (
+            <li key={item.children}>
+              <Link href={item.href}>
+                {item.children}
+              </Link>
+            </li>
+          )
+        })}
       </ul>
     </nav>
   )
--- src/pages/students.tsx
+++ src/pages/students.tsx
@@ -1,7 +1,72 @@
+import { ButtonArea } from "@/components/ButtonArea";
+import { Container } from "@/components/Container";
+import { ContentContainer } from "@/components/ContentContainer";
+import { Form } from "@/components/Form";
+import { FormInputArea } from "@/components/FormInputArea";
+import { InputText } from "@/components/InputText";
+import { SubmitButton } from "@/components/SubmitButton";
+import { Title } from "@/components/Title";
+
 export default function StudentsPage() {
   return (
-    <div className="w-5/6 m-auto mt-20">
-      <h1>Alunos</h1>
-    </div>
+    <Container>
+      <Title text='Alunos' />
+
+      <ContentContainer>
+        <Form onSubmit={() => { }}>
+          <FormInputArea>
+            <InputText
+              name="nome"
+              id="nome"
+              label="Nome"
+              htmlFor="nome"
+            />
+            <InputText
+              name="email"
+              id="email"
+              label="E-mail"
+              htmlFor="email"
+            />
+            <InputText
+              name="rg"
+              id="rg"
+              label="RG"
+              htmlFor="rg"
+            />
+            <InputText
+              name="cpf"
+              id="cpf"
+              label="CPF"
+              htmlFor="cpf"
+            />
+            <InputText
+              name="endereço"
+              id="endereço"
+              label="Endereço"
+              htmlFor="endereço"
+            />
+            <InputText
+              name="contato"
+              id="contato"
+              label="Contato"
+              htmlFor="contato"
+            />
+            <InputText
+              name="objetivo"
+              id="objetivo"
+              label="Objetivo"
+              htmlFor="objetivo"
+            />
+          </FormInputArea>
+
+          <ButtonArea>
+            <SubmitButton>
+              Cadastrar
+            </SubmitButton>
+
+          </ButtonArea>
+        </Form>
+      </ContentContainer>
+    </Container>
   )
 }
