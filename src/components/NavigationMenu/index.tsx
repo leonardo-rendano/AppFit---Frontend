8d748fdb638aeb7c47d02810d1bcc0a6317f40b5
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-03-13 15:11:40 -0300

    menuItems refactor

--- src/components/NavigationMenu/index.tsx
+++ src/components/NavigationMenu/index.tsx
@@ -1,26 +1,7 @@
 import Link from "next/link"
+import { menuItems } from "./menuItems"
 
 export const NavigationMenu = () => {
-
-  const menuItems = [
-    {
-      href: '/teachers',
-      children: 'Professores'
-    },
-    {
-      href: '/students',
-      children: 'Alunos'
-    },
-    {
-      href: '/sets',
-      children: 'Séries'
-    },
-    {
-      href: '/exercises',
-      children: 'Exercícios'
-    }
-  ]
-
   return (
     <nav className="w-1/3">
       <ul className="flex items-center justify-between font-medium">
---
+++ src/components/NavigationMenu/menuItems.ts
@@ -0,0 +1,18 @@
+export const menuItems = [
+  {
+    href: '/teachers',
+    children: 'Professores'
+  },
+  {
+    href: '/students',
+    children: 'Alunos'
+  },
+  {
+    href: '/sets',
+    children: 'Séries'
+  },
+  {
+    href: '/exercises',
+    children: 'Exercícios'
+  }
+]
