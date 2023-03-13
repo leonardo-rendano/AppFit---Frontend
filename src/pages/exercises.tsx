26e00e11593fd71a959601f117be8784bec39c12
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-03-13 15:22:13 -0300

    exercise page finish

--- src/pages/exercises.tsx
+++ src/pages/exercises.tsx
@@ -1,7 +1,40 @@
+import { ButtonArea } from "@/components/ButtonArea";
+import { Container } from "@/components/Container";
+import { ContentContainer } from "@/components/ContentContainer";
+import { Form } from "@/components/Form";
+import { FormInputArea } from "@/components/FormInputArea";
+import { InputText } from "@/components/InputText";
+import { SubmitButton } from "@/components/SubmitButton";
+import { Title } from "@/components/Title";
+
 export default function ExercisesPage() {
   return (
-    <div className="w-5/6 m-auto mt-20">
-      <h1>Exercícios</h1>
-    </div>
+    <Container>
+      <Title text="Exercícios" />
+
+      <ContentContainer>
+        <Form onSubmit={() => {}}>
+          <FormInputArea>
+            <InputText 
+              name="name"
+              id="name"
+              htmlFor="name"
+              label="Exercício"
+            />
+            <InputText 
+              name="reps"
+              id="reps"
+              htmlFor="reps"
+              label="Repetições"
+            />
+          </FormInputArea>
+          <ButtonArea>
+            <SubmitButton>
+              Cadastrar
+            </SubmitButton>
+          </ButtonArea>
+        </Form>
+      </ContentContainer>
+    </Container>
   )
 }
