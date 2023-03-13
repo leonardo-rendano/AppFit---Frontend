3a671dcebb7f9e6bf091e6e654f5633928eb4358
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-03-13 15:18:32 -0300

    exercise and sets page finish

--- src/pages/sets.tsx
+++ src/pages/sets.tsx
@@ -1,7 +1,41 @@
+import { ButtonArea } from "@/components/ButtonArea";
+import { Container } from "@/components/Container";
+import { ContentContainer } from "@/components/ContentContainer";
+import { Form } from "@/components/Form";
+import { FormInputArea } from "@/components/FormInputArea";
+import { InputText } from "@/components/InputText";
+import { SelectInput } from "@/components/SelectInput";
+import { SubmitButton } from "@/components/SubmitButton";
+import { Title } from "@/components/Title";
+
 export default function SetsPage() {
   return (
-    <div className=" w-5/6 m-auto mt-20 ">
-      <h1>Séries</h1>
-    </div>
+    <Container>
+      <Title text="Séries" />
+
+      <ContentContainer>
+        <Form onSubmit={() => {}}>
+          <FormInputArea>
+            <SelectInput
+              label="Tipo de exercício"
+              id="exercise"
+              htmlFor="exercise"
+              name="exercise"
+            >
+              <option value="hipertrophy">Hipertrofia</option>
+              <option value="strenght training">Treinamento de força</option>
+              <option value="aerobic">Aeróbico</option>
+            </SelectInput>
+          </FormInputArea>
+            
+          <ButtonArea>
+            <SubmitButton>
+              Cadastrar
+            </SubmitButton>
+          </ButtonArea>
+
+        </Form>
+      </ContentContainer>
+    </Container>
   )
 }
